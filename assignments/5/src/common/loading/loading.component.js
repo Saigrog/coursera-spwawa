e992d1170d08681acaefadd67b9ee46409307dd9
Jonathon Harper <jon@jharper.email>
2017-09-03 22:26:59 +0100

    Initial module 5 assignment commit

---
+++ assignments/5/src/common/loading/loading.component.js
@@ -0,0 +1,30 @@
+(function() {
+"use strict";
+
+angular.module('common')
+.component('loading', {
+  template: '<img src="images/spinner.svg" ng-if="$ctrl.show">',
+  controller: LoadingController
+});
+
+
+LoadingController.$inject = ['$rootScope'];
+function LoadingController ($rootScope) {
+  let $ctrl = this;
+  let listener;
+
+  $ctrl.$onInit = function() {
+    $ctrl.show = false;
+    listener = $rootScope.$on('spinner:activate', onSpinnerActivate);
+  };
+
+  $ctrl.$onDestroy = function() {
+    listener();
+  };
+
+  function onSpinnerActivate(event, data) {
+    $ctrl.show = data.on;
+  }
+}
+
+})();
---
+++ assignments/5/src/public/menu-items/menu-items.controller.js
@@ -0,0 +1,13 @@
+(function () {
+"use strict";
+
+angular.module('public')
+.controller('MenuItemsController', MenuItemsController);
+
+MenuItemsController.$inject = ['menuItems'];
+function MenuItemsController(menuItems) {
+  let $ctrl = this;
+  $ctrl.menuItems = menuItems;
+}
+
+})();
